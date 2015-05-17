622ea2be2bbeb22f6ae127d8c8b9435fdbf083bd
Jon Thornburg <reliable2192@gmail.com>
2015-05-16 21:41:11 -0600

    Initial Commit

---
+++ ThreeCardMonte/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreeCardMonte
+//
+//  Created by jonathan thornburg on 5/16/15.
+//  Copyright (c) 2015 jonathan thornburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
